9bfb235421f47042a905b26517f1d31199b32a9d
Pallavi Singh <pallavidsingh@outlook.com>
2025-06-02 14:56:06 +0200

    local host

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2/promise");
 
 async function connectDB() {
   const connection = await mysql.createConnection({
-    host:  "10.80.76.54"
+    host:  "10.80.72.24"
     ,user: "root",
     password: "root",
     database: "Usersdb"
